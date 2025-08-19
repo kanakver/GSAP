07e086c85a012c75fe03f89d037b946e23f95faf
kanakver <kanakv008@gmail.com>
2025-08-20 04:16:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useGSAP } from '@gsap/react'
+// import { ScrollTrigger, SplitText } from 'gsap/ScrollTrigger'
+// import gsap from 'gsap'
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex-center'>
+      <h1 className='text-3xl font-bold underline '>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
