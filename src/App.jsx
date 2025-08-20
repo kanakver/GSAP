c2fd2b957edb26fb9be67e31f071a89fa19d708f
kanakver <kanakv008@gmail.com>
2025-08-21 02:48:30 +0530

    fixed the deployment error

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { ScrollTrigger, SplitText } from 'gsap/all';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
 
